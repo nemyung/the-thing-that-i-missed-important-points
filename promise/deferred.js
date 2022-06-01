74f18bd224bc04cba0218f8aff375b65b072f07e
semyung oh <semyung.dev@gmail.com>
2022-06-01 23:27:24 +0900

    feat: update micro task queue's flow for deferred promise

--- promise/deferred.js
+++ promise/deferred.js
@@ -15,21 +15,29 @@ const ref = {
 };
 const { resolve, reject, promise } = defer();
 
+Promise.resolve().then(() => {
+  console.log(`formerMicroTaskQueueStart`);
+});
+
 for (let i = 0; i < 10; i++) {
   promise.then(() => {
+    console.log(`latterMicroTaskQueueStart`);
     console.log(`currentRef: ${++ref.current}`);
+    if (i + 1 === 10) {
+      setTimeout(() => {
+        console.log(`latterMacroTaskQueue`);
+      }, 1000);
+    }
   });
 }
 
-setTimeout(() => {
-  console.log(`This will be queued into macro task queue immediately`);
-}, 0);
+Promise.resolve().then(() => {
+  console.log(`formerMicroTaskQueueEnd`);
+});
 
 setTimeout(() => {
   resolve();
-  console.log(
-    `This will be queued into macro task queue after 2 seconds and resolve promise`
-  );
+  console.log(`formerMacroTaskQueue`);
 }, 2000);
 
 console.log("script end");
